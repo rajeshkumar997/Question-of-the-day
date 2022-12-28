b2afa69660314c6b5007b4181c810f3275f07e69
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2022-12-28 09:25:56 +0530

    Create (28-dec-2022).java

---
+++ LeetCode/(28-dec-2022).java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minStoneSum(int[] piles, int k) {
+        
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
+        
+        for(int i=0;i<piles.length;i++){
+            pq.add(piles[i]);
+        }
+              
+        for(int i=0;i<k;i++){
+            int temp=pq.poll();
+            int res=temp-(temp/2);
+            pq.add(res);
+        }
+        
+        int result=0;
+        for(int i=0;i<piles.length;i++){
+            result+=pq.poll();
+        }
+        
+        return result;
+    }
+}
