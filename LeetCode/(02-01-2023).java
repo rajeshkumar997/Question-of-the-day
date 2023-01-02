90f8600c7084d361bd5e8a6ce758e034135ad3d3
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-02 11:54:02 +0530

    Create (02-01-2023).java

---
+++ LeetCode/(02-01-2023).java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() == 1) return true;
+        if (word.equals(word.toUpperCase())) return true;
+        if (word.equals(word.toLowerCase())) return true;
+        if (Character.isUpperCase(word.charAt(0)) && word.substring(1).equals(word.substring(1).toLowerCase())) return true;
+        return false;
+    }
+}
