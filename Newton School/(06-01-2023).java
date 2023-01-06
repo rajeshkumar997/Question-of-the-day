64d8b39064c1f8dd988592cffe8f180c47f49df5
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-06 11:52:33 +0530

    Create (06-01-2023).java

---
+++ Newton School/(06-01-2023).java
@@ -0,0 +1,25 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int ans = 0; 
+        int sockets = 1;
+
+        while (sockets < b) {
+            sockets += a; 
+            ans++; 
+        }
+        System.out.println(ans);
+    }
+}
+
+
+//       but one testcase is not passing
