7e3d57a56ecb7a8b3cb0a97c0bc7da279f87a7e5
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-07 20:18:37 +0530

    Create (07-01-2023).java

---
+++ GeeksForGeeks/(07-01-2023).java
@@ -0,0 +1,53 @@
+/*Node class  used in the program
+class Node
+{
+	int data;
+	Node next;
+	Node bottom;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+		bottom = null;
+	}
+}
+*/
+/*  Function which returns the  root of 
+    the flattened linked list. */
+class GfG
+{
+    Node mergeTwoLists(Node a, Node b){
+        Node temp = new Node(0);
+        Node res = temp;
+        
+        while(a!=null && b!=null){
+            if(a.data <= b.data){
+                temp.bottom = a;
+                temp = temp.bottom;
+                a = a.bottom;
+            }
+            else{
+                temp.bottom = b;
+                temp = temp.bottom;
+                b = b.bottom;
+            }
+        }
+        if(a != null)
+        temp.bottom = a;
+        else
+        temp.bottom = b;
+        return res.bottom;
+    }
+    Node flatten(Node root)
+    {
+	// Your code here
+	if(root == null || root.next == null)
+	return root;
+	root.next = flatten(root.next);
+	
+	root = mergeTwoLists(root, root.next);
+	
+	    return root;
+    }
+}
