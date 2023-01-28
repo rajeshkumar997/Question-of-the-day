6a181fdbece538c0832206439e38fb318c29709a
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-01-28 10:28:36 +0530

    Create (28-01-2022).java

---
+++ Newton School/(28-01-2022).java
@@ -0,0 +1,25 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    
+        int missing = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == missing) {
+                missing++;
+                i = -1;
+            }
+        }
+        System.out.println(missing);
+    }
+}
