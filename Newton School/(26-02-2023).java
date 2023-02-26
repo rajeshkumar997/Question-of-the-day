aa48b96f48a2a7f605893f5dab9ad90ce643ba51
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-02-26 10:28:58 +0530

    Create (26-02-2023).java

---
+++ Newton School/(26-02-2023).java
@@ -0,0 +1,17 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int x = scanner.nextInt();
+
+        int pos = (x - 1) / n + (int) 'A';
+        char c = (char) pos;
+        System.out.println(c);
+    }
+}
