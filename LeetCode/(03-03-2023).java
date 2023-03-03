ba4ee3fc18ed8f801485a7f2df6c9a3f2cb87ab5
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-03-03 11:08:25 +0530

    Create (03-03-2023).java

---
+++ LeetCode/(03-03-2023).java
@@ -0,0 +1,10 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack.contains(needle)){
+            return haystack.indexOf(needle);
+        }
+        else{
+            return -1;
+        }
+    }
+}
