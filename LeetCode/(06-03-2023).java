0f56fe14046c41a3d7cec254707f2d876adc2217
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-03-06 11:19:44 +0530

    Create (06-03-2023).java

---
+++ LeetCode/(06-03-2023).java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        // for(int i : arr){
+        //     if(i <= k) k++; else break;
+        // }
+        // return k;
+
+         int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] - mid - 1 < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left + k;
+    }
+}
