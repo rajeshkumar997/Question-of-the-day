a2e2d1d2bc75a56fea3e51c56e63f43f5dd390e9
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-03-09 11:15:36 +0530

    Create  Linked List Cycle II (09-03-2023).java

---
+++ LeetCode/Linked List Cycle II (09-03-2023).java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        while(head !=null && set.add(head)){
+            head = head.next;
+        }
+        return head;
+    }
+}
