12bffa733a2c50b6bc123e237af21ad02d8232be
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-03-15 17:38:38 +0530

    Create

---
+++ Newton School/Alexa and Drink (15-03-2023).java
@@ -0,0 +1,16 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        double res = (a*b)/100.0;
+        System.out.printf("%.2f", res);
+    }
+}
