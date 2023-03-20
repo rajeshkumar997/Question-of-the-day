4894ddcf5a21ff584d391f7444ce57b7e87435c8
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-03-20 10:53:13 +0530

    Create Distinct Lottery

---
+++ Newton School/Distinct Lottery (20-03-2023).java
@@ -0,0 +1,21 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) throws IOException {
+        // Your code here
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        HashMap<String, Integer> itemCounts = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            String item = reader.readLine();
+            int count = itemCounts.getOrDefault(item, 0);
+            itemCounts.put(item, count + 1);
+        }
+        System.out.println(itemCounts.size());
+    }
+}
