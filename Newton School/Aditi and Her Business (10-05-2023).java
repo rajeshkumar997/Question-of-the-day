cfc9b9352f5bbb9dde57725c9b250406a89bbcbc
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-05-10 12:45:59 +0530

    Create Aditi and Her Business (10-05-2023).java

---
+++ Newton School/Aditi and Her Business (10-05-2023).java
@@ -0,0 +1,28 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int maxLength = 1;
+        int currentLength = 1;
+        for (int i = 1; i < n; i++) {
+            if (a[i] >= a[i-1]) {
+                currentLength++;
+            } else {
+                maxLength = Math.max(maxLength, currentLength);
+                currentLength = 1;
+            }
+        }
+        maxLength = Math.max(maxLength, currentLength);
+        System.out.println(maxLength);
+    }
+}
