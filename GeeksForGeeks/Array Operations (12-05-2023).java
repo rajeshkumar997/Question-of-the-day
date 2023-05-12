599143d329b125e7c57b58d4a0f1ce5e994b4369
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-05-12 11:49:29 +0530

    Array Operations (12-05-2023).java

---
+++ GeeksForGeeks/Array Operations (12-05-2023).java
@@ -0,0 +1,18 @@
+class Solution {
+    public static int arrayOperations(int n, int[] arr) {
+        // code here
+        int ans = 0;
+        int zerocnt = 0;
+        for(int i = 0; i < n; ++i){
+            if(arr[i] == 0){
+                zerocnt++;
+                if(i > 0 && arr[i-1] != 0) ans++;
+            }
+        }    
+        if(arr[n-1] != 0) ans++;
+        
+        if(zerocnt == n) return 0;
+        if(zerocnt == 0) return -1;
+        return ans;
+    }
+}
