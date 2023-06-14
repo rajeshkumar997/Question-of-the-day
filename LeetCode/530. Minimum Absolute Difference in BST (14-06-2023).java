d10c7f7f5b2c346ed3b33cb2615bbc9b537ef407
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-06-14 18:11:57 +0530

    530. Minimum Absolute Difference in BST (14-06-2023).java

---
+++ LeetCode/530. Minimum Absolute Difference in BST (14-06-2023).java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int minDifference = Integer.MAX_VALUE;
+    TreeNode prevNode;
+
+    void inorderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inorderTraversal(node.left);
+        if (prevNode != null) {
+            minDifference = Math.min(minDifference, node.val - prevNode.val);
+        }
+        prevNode = node;
+        inorderTraversal(node.right);
+    }
+
+    int getMinimumDifference(TreeNode root) {
+        inorderTraversal(root);
+        return minDifference;
+    }
+}
