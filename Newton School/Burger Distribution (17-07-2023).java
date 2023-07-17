ec477ac89c68f8d02ec0be5f74eca12f52a2fb47
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-07-17 11:02:06 +0530

    Burger Distribution (17-07-2023).java

---
+++ Newton School/Burger Distribution (17-07-2023).java
@@ -0,0 +1,19 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if(n >= 55){
+            System.out.print("yes");
+        }
+        else{
+            System.out.print("no");
+        }
+    }
+}
