682f36a699025e676ed971a05bad453b40b7b2fd
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-08-16 17:01:29 +0530

    Vikas and his prime numbers (16-08-2023).java

---
+++ Newton School/Vikas and his prime numbers (16-08-2023).java
@@ -0,0 +1,36 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> primes = findPrimes(n);
+        for (int prime : primes) {
+            System.out.print(prime + " ");
+        }
+    }
+    public static List<Integer> findPrimes(int n) {
+        List<Integer> primes = new ArrayList<>();
+        boolean[] isPrime = new boolean[n + 1];
+        for (int i = 2; i <= n; i++) {
+            isPrime[i] = true;
+        }
+        for (int i = 2; i * i <= n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j <= n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (isPrime[i]) {
+                primes.add(i);
+            }
+        }
+        return primes;
+    }
+}
