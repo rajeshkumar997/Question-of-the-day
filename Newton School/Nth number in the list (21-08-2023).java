dd40c1971c75e40232935c3ba819e099490a52b9
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-08-21 11:13:30 +0530

    Nth number in the list (21-08-2023).java

---
+++ Newton School/Nth number in the list (21-08-2023).java
@@ -0,0 +1,15 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int res = (int)(Math.pow(n, 3) - Math.pow(n,2));
+        System.out.print(res);
+    }
+}
