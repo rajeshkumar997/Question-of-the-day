c470fea7c1c27752f0d3f2fd08c7a20dd780ff82
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-08-24 11:27:59 +0530

    Different characters (24-08-2023).java

---
+++ Newton School/Different characters (24-08-2023).java
@@ -0,0 +1,21 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine().toLowerCase();
+        String b = sc.nextLine().toLowerCase();
+        
+        int count = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
