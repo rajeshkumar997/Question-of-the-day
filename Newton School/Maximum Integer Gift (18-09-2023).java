d7a35539ca7c69513faead504fff452a6ecbad92
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-09-18 14:02:08 +0530

    Maximum Integer Gift (18-09-2023).java

---
+++ Newton School/Maximum Integer Gift (18-09-2023).java
@@ -0,0 +1,16 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        int res = Math.max(Math.max((x+y),(x-y)), x*y);
+        System.out.print(res);
+    }
+}
