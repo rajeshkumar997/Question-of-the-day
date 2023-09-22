90ded7ba570dae91750ca4970b1d81908a8a1300
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-09-22 10:07:17 +0530

    Edward and Toys (22-09-2023).java

---
+++ Newton School/Edward and Toys (22-09-2023).java
@@ -0,0 +1,23 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int count = 0;
+        for(int i=(n-k); i<n; i++){
+            count += arr[i];
+        }
+        System.out.print(count);
+    }
+}
