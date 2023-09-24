459e67c9f195cb6d0d32a379edc3b5f43f192d76
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-09-24 14:56:52 +0530

    Add or Subtract (24-09-2023).java

---
+++ Newton School/Add or Subtract (24-09-2023).java
@@ -0,0 +1,15 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        System.out.print(y%x == 0 ? x+y : y-x);
+    }
+}
