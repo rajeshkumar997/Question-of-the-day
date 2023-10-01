604fcc8e420119d4d54862bec473840dc01f2fa3
Rajesh kumar <113416194+rajeshkumar997@users.noreply.github.com>
2023-10-01 23:39:11 +0530

    557. Reverse Words in a String III (01-10-2023).js

---
+++ LeetCode/557. Reverse Words in a String III (01-10-2023).js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    const words = s.split(' ');
+    for(let i = 0; i<words.length; i++){
+        words[i] = words[i].split('').reverse().join('');
+    }
+    return words.join(' ');
+};
